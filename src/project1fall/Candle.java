b3720b0aa7754b7035bd827126a2bb4e3f90899f
kevin95822 <kevin95822@hotmail.com>
2019-11-12 15:43:28 -0500

    Add files via upload

---
+++ src/project1fall/Candle.java
@@ -0,0 +1,50 @@
+package project1fall;
+
+public class Candle {
+	  private int height,width;
+	  private float price;
+	  private boolean lit;
+
+	  //3 parameter constructor
+	  public Candle(int h, int w, float p){
+	    h = height;
+	    w = width;
+	    p = price;
+	  }
+	  
+	  public Candle(String string1, String string2, String string3) {
+		  string1 = Integer.toString(height);
+		  string2 = Integer.toString(width);
+		  string3 = Float.toString(price);
+	  }
+	  
+	  //get method
+	  public int getHeight(){return height;}
+
+	  public int getWidth(){return width;}
+
+	  public float getPrice(){return price;}
+
+	  public boolean getLit(){return lit;}
+
+	  //set method
+	  public void setHeight(int h){
+		  h = height;
+	  }
+	  
+	  public void setWidth(int w) {
+		  w = width;
+	  }
+	  
+	  public void setPrice(float p) {
+		  p = price;
+	  }
+	  
+	  public void setLit(boolean l) {
+		  l = lit;
+	  }
+	  //show candle object as a string 
+	  public String toString() {
+		  return height + "," + width + "," + price;
+	  }
+}
